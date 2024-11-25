780fc1b2c44925c42c3bce4e7c52aab84e891070
marcheloseII <asegurae@ucenfotec.ac.cr>
2024-11-24 22:49:01 -0600

    Fix gestionado
    Faltaria bloquear los campos de texto

--- src/app/components/contact/contact-form/contact-form.component.ts
+++ src/app/components/contact/contact-form/contact-form.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { IContact } from '../../../interfaces';
+import { AuthService } from '../../../services/auth.service'; // Importa el servicio de autenticación
 
 @Component({
   selector: 'app-contact-form',
@@ -13,15 +14,31 @@ import { IContact } from '../../../interfaces';
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.scss'
 })
-export class ContactFormComponent {
+export class ContactFormComponent implements OnInit {
 
   @Input() title: string = '';
   @Input() contactForm!: FormGroup;
   @Output() callSendMethod: EventEmitter<IContact> = new EventEmitter<IContact>();
 
+  // Inyecta el servicio de autenticación
+  private authService = inject(AuthService);
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    const user = this.authService.getUser();
+
+    // Verifica si el usuario existe antes de inicializar el formulario
+    this.contactForm = this.fb.group({
+      name: [user?.name || ''],    // Acceso seguro con `?.`
+      email: [user?.email || ''],  // Acceso seguro con `?.`
+      subject: [''],                // Campo vacío por defecto
+      message: ['']                 // Campo vacío por defecto
+    });
+  }
 
   sendEmail() {
-    console.log('Save button clicked')
+    console.log('Save button clicked');
     let contact: IContact = {
       name: this.contactForm.controls['name'].value,
       email: this.contactForm.controls['email'].value,
