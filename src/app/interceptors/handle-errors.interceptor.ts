549bea427d24967f1caca74309d1d229dd8e50a2
marcheloseII <asegurae@ucenfotec.ac.cr>
2024-12-06 20:18:12 -0600

    Validacion error login

--- src/app/interceptors/handle-errors.interceptor.ts
+++ src/app/interceptors/handle-errors.interceptor.ts
@@ -3,18 +3,49 @@ import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable, catchError, of } from 'rxjs';
 import { AuthService } from '../services/auth.service';
+import { AlertService } from '../services/alert.service';
 
 export const handleErrorsInterceptor: HttpInterceptorFn = (req, next) => {
   const router: Router = inject(Router);
   const authService: AuthService = inject(AuthService);
+  const alertService: AlertService = inject(AlertService);
 
   return next(req).pipe(
     catchError((error: any): Observable<any> => {
-      if ((error.status === 401 || error.status === 403) && !req.url.includes('auth')) {
+      
+      let errorMessage = 'Error de login.';
+      console.log('Error de login HANDLE ERRORS.SERVICE:', error);
+      console.log('Request URL:', req.url);
+      
+      // if ((error.status === 401 || error.status === 403) && !req.url.includes('auth')) {
+
+      if ((error.status === 401 ) && req.url.includes('auth')) { //!req.url.includes('auth') antes
+        errorMessage = 'Error, revisar información de usuario';
+        alertService.displayAlert(
+          'error',
+          errorMessage,
+          'center',
+          'top',
+          ['error-snackbar']
+        );
         authService.logout();
         router.navigateByUrl('/login');
         return of({ status: false });
       }
+      if ((error.status === 403) && req.url.includes('auth')){
+        errorMessage = 'Usuario deshabilitado';
+        alertService.displayAlert(
+          'error',
+          errorMessage,
+          'center',
+          'top',
+          ['error-snackbar']
+        );
+        authService.logout();
+        router.navigateByUrl('/login');
+        return of({ status: false });
+      }
+
       if (error.status === 422) {
         throw error.error;
       }
@@ -22,6 +53,6 @@ export const handleErrorsInterceptor: HttpInterceptorFn = (req, next) => {
         throw { status: false };
       }
       return of({ status: false });
-    })
-  );
+    })
+  );
 };
